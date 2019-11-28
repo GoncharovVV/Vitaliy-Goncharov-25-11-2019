8f816576cbcfedc6a5d7baa2023f4f964dfe6cc3
Vitaliy <goncharov.v.v2017@gmail.com>
2019-11-28 18:19:58 +0200

    consumer added

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import { AsyncTypeahead } from 'react-bootstrap-typeahead';
-import WeatherService from '../../services/WeatherService';
 import { ICity } from '../../utils/types';
 import './Search.scss';
+import { WeatherServiceConsumer } from '../WeatherServiceContext';
 export interface SearchProps {}
 
 const Search: React.FC<SearchProps> = () => {
-  const weatherService = new WeatherService();
-  const { getCities } = weatherService;
-
   const [isLoading, setisLoading] = React.useState<boolean>(false);
   const [options, setOptions] = React.useState<Array<ICity>>([{
     id: '323903',
@@ -19,32 +16,36 @@ const Search: React.FC<SearchProps> = () => {
   const multiple = React.useState<boolean>(false);
   const allowNew = React.useState<boolean>(false);
   return (
-
-      <AsyncTypeahead
-        id="typehad-id"
-        isLoading={isLoading}
-        allowNew={allowNew[0]}
-        multiple={multiple[0]}
-        placeholder="Search for a Github user..."
-        onChange={(selected: Array<ICity>) => {
-          if (selected.length>0) {
-            console.log(selected[0].id);
-          }
-        }}
-        onSearch={(query: string) => {
-          setisLoading(true);
-          getCities(query).then(res => {
-            setisLoading(false);
-            setOptions(prev => {
-
-              return res;
-            });
-          });
-        }}
-        labelKey={option => `${option.localizedName} (${option.country} / ${option.administrative})`}
-        options={options}
-      />
-    );
-  }
+    <WeatherServiceConsumer>
+      {({ getCities }: any) => {
+        return (
+          <AsyncTypeahead
+            id="typehad-id"
+            isLoading={isLoading}
+            allowNew={allowNew[0]}
+            multiple={multiple[0]}
+            placeholder="Search for a Github user..."
+            onChange={(selected: Array<ICity>) => {
+              if (selected.length>0) {
+                console.log(selected[0].id);
+              }
+            }}
+            onSearch={(query: string) => {
+              setisLoading(true);
+              getCities(query).then((res: any) => {
+                setisLoading(false);
+                setOptions(() => {
+                  return res;
+                });
+              });
+            }}
+            labelKey={option => `${option.localizedName} (${option.country} / ${option.administrative})`}
+            options={options}
+          />
+        )
+      }}
+    </WeatherServiceConsumer>
+  );
+}
 
 export default Search;
