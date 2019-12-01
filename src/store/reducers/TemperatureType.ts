6c010dac0d726755f846ef52edbc23b0e2faeb0e
vitaliy.g <vitaliy.g@ideasoft.io>
2019-12-01 15:27:49 +0200

    temp store created

---
+++ src/store/reducers/TemperatureType.ts
@@ -0,0 +1,15 @@
+const updateTemperatureType = (state: any, action: any): string => {
+  if (!state) {
+    return 'F'
+  };
+  switch(action.type) {
+    case 'TEMPERATURE_TYPE_IMP':
+      return 'F';
+    case 'TEMPERATURE_TYPE_METR':
+      return 'C';
+    default:
+      return state.temperatureType
+  }
+};
+
+export default updateTemperatureType;
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,12 +1,14 @@
 import updateFavouritesList from './FavouritesList';
 import updateCurrentCity from './CurrentCity';
 import updateWetherList from './WeatherList';
+import updateTemperatureType from './TemperatureType';
 
 const reducer = (state: any, action: any) => {
   return {
     favouritesList: updateFavouritesList(state, action),
     currentCity: updateCurrentCity(state, action),
-    weatherList: updateWetherList(state, action)
+    weatherList: updateWetherList(state, action),
+    temperatureType: updateTemperatureType(state, action)
   }
 };
 
